42d5a393736bc19efb022bb04f7d5ab408a9b11c
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-10 14:09:41 +0100

    initial E1

--- E1/main.c
+++ E1/main.c
@@ -12,7 +12,20 @@
  * 
  */
 int main(int argc, char** argv) {
-
+    int nb_client;
+    int port;
+    char ** addr;
+    if (argc <!=3 ) {
+        printf("Use : %s nb_client port",argv[0]);
+        return(1);
+    } else {
+        nb_client = atoi(argv[1]);
+        addr = calloc(nb_client,19*sizeof(char));
+        addr = ping(nb_client,port);
+        for (int i =0 ; i < 3 ; i++ ) {
+            printf("%s\r\n",nb_client[i]);
+        }
+    }
     return (EXIT_SUCCESS);
 }
 
