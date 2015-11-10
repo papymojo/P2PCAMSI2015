36abe80262461ff9fec308137b0e5ab770dda822
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-10 14:46:57 +0100

    main

--- E1/main.c
+++ E1/main.c
@@ -7,23 +7,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "net_interface_udp.h"
 /*
  * 
  */
 int main(int argc, char** argv) {
     int nb_client;
     int port;
-    char ** addr;
+    char **addr;
     if (argc <!=3 ) {
         printf("Use : %s nb_client port",argv[0]);
         return(1);
     } else {
         nb_client = atoi(argv[1]);
-        addr = calloc(nb_client,19*sizeof(char));
         addr = ping(nb_client,port);
-        for (int i =0 ; i < 3 ; i++ ) {
-            printf("%s\r\n",nb_client[i]);
+        for (int i =0 ; i < nb_client ; i++ ) {
+            printf("%s\r\n",addr[i]);
         }
     }
     return (EXIT_SUCCESS);
