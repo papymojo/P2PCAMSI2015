04b60de827c081cbaeb2e22ebd48750ee4875635
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-10 13:51:14 +0100

    initial E1

---
+++ E1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: benjaminbercy
+ *
+ * Created on 10 novembre 2015, 13:50
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
