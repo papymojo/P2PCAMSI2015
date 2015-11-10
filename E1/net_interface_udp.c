de91f3793e734badf8eb1829e59172d199806ee2
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-10 15:32:31 +0100

    multicast

--- E1/net_interface_udp.c
+++ E1/net_interface_udp.c
@@ -12,12 +12,25 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-char** p2p_ping(int nb_client) {
+struct sockaddr_in data;
+int fd;
+char** p2p_ping(int nb_client,int port) {
     char** addr;
     
     addr = calloc(nb_client, sizeof(char*));
     
+    //open udp
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+    memset(&addr,0,sizeof(addr));
+    data.sin_family = AF_INET;
+    data.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
+    data.sin_port=htons(port);
     for (int i=0 ; i < nb_client ; i++ ) {
+        
         addr[i] = calloc(19, sizeof(char));
         strncpy(addr[i], "127.0.0.1", 19);
     }
@@ -26,22 +39,11 @@ char** p2p_ping(int nb_client) {
 }
 
 int p2p_send(char* buffer, int size, int port) {
-    struct sockaddr_in si_me, si_other;
-    
-    int sock;
-    
-    /* create an UDP socket */
-    if (-1 == (sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))) {
-        perror("Fail on socket creation");
-        exit(-1);
-    }
-    
-    /* init structure socket */
-    memset((char* ) &si_me, 0, sizeof(si_me));
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = 
-    
-    
+            if (sendto(fd, buffer, size, 0,(struct sockaddr *) &data, sizeof(data)) < 0)
+        {
+            perror("sendto");
+            exit(1);
+        }
     return 0;
 }
 
