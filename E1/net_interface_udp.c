be0727f8293a3765a9115886bbdfe6e2afcb43bb
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-12 11:30:19 +0100

    p2p3

--- E1/net_interface_udp.c
+++ E1/net_interface_udp.c
@@ -90,10 +90,9 @@ char** p2p_ping(int nb_client,char* ip,int port) {
         else {
             -- i;
         }
-        usleep(5);
         p2p_send(ip,19);
     }
-    
+    p2p_send(ip,19); 
     return addr;
 }
 
