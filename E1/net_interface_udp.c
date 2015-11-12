e55d18b396b21a6f6e33c1d5b4490ae2e9a79c29
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-12 10:30:03 +0100

    p2p1

--- E1/net_interface_udp.c
+++ E1/net_interface_udp.c
@@ -26,7 +26,7 @@ char** p2p_ping(int nb_client,char* ip,int port) {
     addr = calloc(nb_client, sizeof(char*));
     buffer = calloc(19,sizeof(char));
     
-    //open udp
+    //open udp client
     if ((fdc = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket");
@@ -39,10 +39,27 @@ char** p2p_ping(int nb_client,char* ip,int port) {
         perror("setsockopt");
         exit(1);
     }
+    
     memset(&datac,0,sizeof(datac));
     datac.sin_family = AF_INET;
     datac.sin_addr.s_addr = inet_addr(MULTICAST_ADDR);
     datac.sin_port=htons(port);
+    
+    //open udp server
+    if ((fds = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    datas.sin_addr.s_addr = htonl(INADDR_ANY);
+    datas.sin_family = AF_INET;
+    datas.sin_port = htons(port);
+    if(bind (fds, &datas, sizeof(datas)) < 0)
+    {
+        perror("bind");
+        exit(errno);
+    }
     
     //communication (envoi de l'addresse dans le reseau)
     addr[0] = calloc(19, sizeof(char));
@@ -78,7 +95,7 @@ int p2p_send(char* buffer, int size) {
 
 int p2p_recieve(void* buffer, int size) {
     socklen_t datasize = (socklen_t) sizeof(datac);
-    if ( recvfrom( fdc, buffer, size, 0, (struct sockaddr *) &datac, &datasize) < 0) {
+    if ( recvfrom( fds, buffer, size, 0, (struct sockaddr *) &datac, &datasize) < 0) {
         perror("recevievefrom");
         exit(1);
     }
