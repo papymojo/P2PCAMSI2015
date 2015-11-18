76458eb8fff4b057537978e6217479e7a43e9470
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-11-18 15:20:27 +0100

    serveur fichier

---
+++ E2s/net_interface_tcp.c
@@ -0,0 +1,57 @@
+/*
+ * File: net_interface_udp.c
+ * Author: Olivier
+ * 
+ * Created on 10 novembre 2015, 13:50
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "net_interface_tcp.h"
+
+struct sockaddr_in datas;
+struct sockddr datac;
+int fdl;
+int fdc;
+int f;
+
+int tcps_file(char * file,char * ip,int port) {
+    if ( (fdl = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+	fprintf(stderr, "socket : \n");
+	exit(EXIT_FAILURE);
+    }
+    memset(&datas, 0, sizeof(struct sockaddr_in));
+    datas.sin_family      = AF_INET;
+    datas.sin_addr.s_addr = htonl(INADDR_ANY);
+    datas.sin_port        = htons(port);
+    
+    if ( bind(fdl, (struct sockaddr *) &datas, sizeof(datas)) < 0 ) {
+	fprintf(stderr, "bind : \n");
+	exit(EXIT_FAILURE);
+    }
+    if ( listen(fdl,MAX_LINE) < 0 ) {
+	fprintf(stderr, "listen : \n");
+	exit(EXIT_FAILURE);
+    }
+    if (accept(fdl, &datac, sizeof(datac)) < 0) {
+        fprintf(stderr,"accept : \n");
+    }
+    
+
+}
+
+int tcps_send(char* buffer, int size) {
+    recv(fdc, (void *) buffer, size, 0);
+    return 0;
+}
+
+int tcps_send(char* buffer, int size) {
+    send(fdc, (const void *)buffer,size, 0);
+    return 0;
+}
+
