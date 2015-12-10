45cd60ef256ecd1833998294588845513378ba06
benjaminbercy <benjaminbercy@benjaminbercy-Lenovo-Y50-70>
2015-12-10 11:03:22 +0100

    EF

---
+++ EF/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: benjaminbercy
+ *
+ * Created on 10 décembre 2015, 11:03
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
